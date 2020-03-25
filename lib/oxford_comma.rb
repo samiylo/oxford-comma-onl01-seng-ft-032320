4a030a64257b3729fddc3d83aaf8383a7fb1b555
Samiylo Kryshu <skryshu@gmail.com>
2020-03-25 09:01:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length < 2 
+    only_item = array[0]
+    
+    
+  elsif array.length == 2
+    array.join(" and ")
+    
+  elsif array.length > 2 
+    last = array[-1]
+    lastNew = "and #{last}"
+    array.pop
+    array << lastNew
+    array.join(", ")
+  end
+  
 end
